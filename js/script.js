b0c49ed97336794ea05a4acd8ec1f6eff25f1c87
e.soheilinia <e.soheilinia@yahoo.com>
2025-09-24 20:18:09 -0300

    Refactor navbar and enhance styles for improved responsiveness and user experience

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // Add custom JavaScript here
+function userScroll() {
+  const nvabar = document.querySelector(".navbar");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      nvabar.classList.add("bg-dark");
+      nvabar.classList.add("navbar-sticky");
+    } else {
+      nvabar.classList.remove("bg-dark");
+      nvabar.classList.remove("navbar-sticky");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userScroll);
